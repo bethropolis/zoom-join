6a739d2b0440e90f79581010b01e810bf2866d3c
bethuel <bethropolis@gmail.com>
2022-03-16 17:51:59 +0300

    green

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,16 +1,16 @@
 const zoomJoin = "zoomy"
 const assets = [
-  "/",
-  "/index.html",
-  "/index.js",
-  "/style.css",
-  "/img/timetable.jpeg",
-  "/https://fonts.googleapis.com/*",
-  "/lib/*",
-  "/lib/font-awesome/font-awesome.min.css",
-  "/lib/vue.js",
-  "/lib/bootstrap5.min.css",
-  "/lib/jquery-3.3.1.min.js"
+  "/zoom-join/",
+  "index.html",
+  "index.js",
+  "style.css",
+  "img/timetable.jpeg",
+  "https://fonts.googleapis.com/*",
+  "lib/*",
+  "lib/font-awesome/font-awesome.min.css",
+  "lib/vue.js",
+  "lib/bootstrap5.min.css",
+  "lib/jquery-3.3.1.min.js"
 ]
 
 self.addEventListener("install", installEvent => {
