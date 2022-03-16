9247f7a893b56bda01e23a2709b41a4e81ba0f2a
Bethuel <bethropolis@gmail.com>
2022-03-16 19:45:53 +0300

    go

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,15 +1,18 @@
 const zoomJoin = "zoomy"
 const assets = [
-  "./",
-  "./index.html",
-  "./index.js",
-  "./style.css",
-  "./img/timetable.jpeg",
-  "./lib/",
-  "./lib/font-awesome/font-awesome.min.css",
-  "./lib/vue.js",
-  "./lib/bootstrap5.min.css",
-  "./lib/jquery-3.3.1.min.js"
+  "./?v2",
+  "serviceWorker.js",
+  "app.webmanifest",
+  "index.html?v2",
+  "index.js",
+  "style.css",
+  "img/timetable.jpeg",
+  "lib/",
+  "lib/font-awesome/font-awesome.min.css",
+  "lib/vue.js",
+  "lib/bootstrap5.min.css",
+  "lib/jquery-3.3.1.min.js",
+  "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;1,400&display=swap"
 ]
 
 self.addEventListener("install", installEvent => {
@@ -19,6 +22,20 @@ self.addEventListener("install", installEvent => {
     })
   )
 }) 
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request)
+          .then(function(response) {
+                  // Cache hit - return response
+                  if (response) {
+                      return response;
+                  }
+                  return fetch(event.request);
+              }
+          )
+  );
+});
 
 self.addEventListener('activate', (event) => {
   // Specify allowed cache keys
